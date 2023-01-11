d9a10422350f46b5d32888aa54cfab8491f3c6c4
dorogit <ishaanjainlock10@gmail.com>
2023-01-11 21:58:26 +0530

    xd

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -4,45 +4,25 @@ class GoogleAuth extends React.Component {
   state = { isSignedIn: null };
 
   componentDidMount() {
+    function HandleToken(response) {
+    console.log(response)
+    }
     window.onload = function () {
       google.accounts.id.initialize({
-        client_id: '  218020527928-33r48v1f5gdup9kg7mbtdsagnjh6j3ki.apps.googleusercontent.com',
-        callback: HandleToken(),
-        scope: 'email'
+        client_id: '218020527928-33r48v1f5gdup9kg7mbtdsagnjh6j3ki.apps.googleusercontent.com',
+        callback:HandleToken,
+        scope: 'email',
+        login_uri:"http://localhost:3000",
       });
-      google.accounts.id.prompt();
+      google.accounts.id.renderButton(
+        document.getElementById("SignIn"),
+        { theme: "outline", size: "large" },
+      );
     };
   }
 
-  HandleToken = (token) => {
-    console.log(token)
-  }
-  renderAuthButton() {
-    return(
-    <div>
-      <div id="g_id_onload"
-        data-client_id="218020527928-33r48v1f5gdup9kg7mbtdsagnjh6j3ki.apps.googleusercontent.com"
-        data-context="signin"
-        data-ux_mode="popup"
-        data-login_uri="http://localhost:3000"
-        data-itp_support="true">
-      </div>
-
-      <div className="g_id_signin"
-        data-type="standard"
-        data-shape="rectangular"
-        data-theme="outline"
-        data-text="signin_with"
-        data-size="large"
-        data-logo_alignment="left">
-      </div>
-      <div className="g_id_signout">Sign Out</div>
-    </div>
-    )
-  }
-
   render() {
-    return <div>{this.renderAuthButton()}</div>;
+    return <div id ="SignIn"></div>;
   }
 }
 
