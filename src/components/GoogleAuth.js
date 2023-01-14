b926a059ea96748a2177034b28be835f0357ca53
dorogit <ishaanjainlock10@gmail.com>
2023-01-14 23:49:20 +0530

    pushed stuff

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -2,25 +2,26 @@ import React from 'react';
 import jwtDecode from 'jwt-decode';
 
 class GoogleAuth extends React.Component {
-  state = { isSignedIn: null };
+  state = { ID: null };
 
   componentDidMount() {
     function HandleToken(response) {
     const decoded = jwtDecode(response.credential)
     console.log(decoded)
     console.log(`This is your ID: ${decoded.sub}`)
+    this.setState({ID: decoded.sub})
     }
     window.onload = function () {
       google.accounts.id.initialize({
         client_id: '218020527928-33r48v1f5gdup9kg7mbtdsagnjh6j3ki.apps.googleusercontent.com',
-        callback:HandleToken,
+        callback:HandleToken.bind(this),
         login_uri:"http://localhost:3000",
       });
       google.accounts.id.renderButton(
         document.getElementById("SignIn"),
         { theme: "outline", size: "large" },
       );
-    };
+    }.bind(this);
   }
 
   render() {
